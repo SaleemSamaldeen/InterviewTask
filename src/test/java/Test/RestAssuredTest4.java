7c0769faabfa8f94593709c3e7d70f8d2cdafdd3
Saleem <saleemsamaldeen@gmail.com>
2022-11-18 05:48:21 +0530

    POM with restAPI

--- src/test/java/Test/RestAssuredTest4.java
+++ src/test/java/Test/RestAssuredTest4.java
@@ -5,10 +5,11 @@ import io.restassured.RestAssured;
 import io.restassured.http.Method;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
+import org.springframework.test.web.reactive.server.WebTestClient;
 import org.testng.annotations.Test;
 
+import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public class RestAssuredTest4 extends BaseTest {
@@ -21,6 +22,8 @@ public class RestAssuredTest4 extends BaseTest {
         RequestSpecification requestSpec = RestAssured.given();
         //Get request method to fetch request details
         Response response = requestSpec.request(Method.GET, parameter);
+        // Example of how to send request with basic authentication credentials
+        //requestSpec.auth().preemptive().basic("","").get(parameter);
 
         System.out.println("Status code: " + response.getStatusCode());
         System.out.println("Status line: " + response.getStatusLine());
@@ -28,9 +31,14 @@ public class RestAssuredTest4 extends BaseTest {
         System.out.println("Status body - Message: " + response.jsonPath().get("message"));
         System.out.println("Status body - page: " + response.jsonPath().get("page"));
         System.out.println("Status body - total: " + response.jsonPath().get("total"));
-        List<HashMap> responseList = response.jsonPath().get("data");
+        ArrayList<HashMap> responseList = response.jsonPath().get("data");
         Map<String, String> support = response.jsonPath().get("support");
         System.out.println(responseList.get(0).get("id"));
+    }
+
+    //webtestclient similar to rest assured used to automate web services
+    protected WebTestClient testClient() {
+        return WebTestClient.bindToServer().build();
     }
 
 }
